68fffb86230242533fee5a93bc28989a807a0417
Dmitry <ryazanovpr@gmail.com>
2020-02-18 23:03:42 +0300

    init

---
+++ BackendWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using BackendApi;
+using Grpc.Net.Client;
+
+namespace aspnetcoreapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async void OnPost()
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:5005");
+            var client = new Job.JobClient(channel);
+            var reply = await client.RegisterAsync(
+                              new RegisterRequest { Description = "This is job" });
+            Console.WriteLine("Job Id: " + reply.Id);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
